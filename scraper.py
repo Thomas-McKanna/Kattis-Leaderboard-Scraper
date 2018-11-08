7b1df9cd9bf703618ed1234d5c67e150d91dccff
Thomas McKanna <thomasmckanna@gmail.com>
2018-11-07 22:34:01 -0600

    Data is now received over network and some parsing.

--- scraper.py
+++ scraper.py
@@ -1,4 +1,23 @@
 import http.client
+from html.parser import HTMLParser
 
 conn = http.client.HTTPSConnection("www.kattis.com", 443)
 conn.request("GET", "https://open.kattis.com/universities/mst.edu")
+response = conn.getresponse()
+
+data = str(response.read())
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if tag == "table":
+            print("Found table!")
+            print(attrs)
+
+    def handle_endtag(self, tag):
+        pass
+
+    def handle_data(self, data):
+        pass
+
+parser = MyHTMLParser()
+parser.feed(data)
