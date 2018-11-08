74f995f074fddd96d4aa0c71015a0935fde6ff9a
Thomas McKanna <thomasmckanna@gmail.com>
2018-11-08 17:53:59 -0600

    Investigated using lxml module.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import http.client
 from html.parser import HTMLParser
+from lxml import etree
+from io import StringIO
 
 conn = http.client.HTTPSConnection("www.kattis.com", 443)
 conn.request("GET", "https://open.kattis.com/universities/mst.edu")
@@ -7,17 +9,12 @@ response = conn.getresponse()
 
 data = str(response.read())
 
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        if tag == "table":
-            print("Found table!")
-            print(attrs)
+decodedData = bytes(data, "utf-8").decode("unicode_escape") 
 
-    def handle_endtag(self, tag):
-        pass
+parser = etree.HTMLParser()
 
-    def handle_data(self, data):
-        pass
+tree   = etree.parse(StringIO(data), parser)
 
-parser = MyHTMLParser()
-parser.feed(data)
+result = etree.tostring(tree.getroot(), method="html")
+
+print(result)
