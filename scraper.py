8055a70ddfeebd5300e74e83d7da00d7c5c082e5
Thomas McKanna <thomasmckanna@gmail.com>
2018-11-12 11:03:26 -0600

    Basic implementation of the scraper complete. Values not yet stored.

--- scraper.py
+++ scraper.py
@@ -11,10 +11,44 @@ data = str(response.read())
 
 decodedData = bytes(data, "utf-8").decode("unicode_escape") 
 
+decodedString = str(decodedData)
+
 parser = etree.HTMLParser()
 
-tree   = etree.parse(StringIO(data), parser)
+tree   = etree.parse(StringIO(decodedString), parser)
 
-result = etree.tostring(tree.getroot(), method="html")
+root = tree.getroot()
 
-print(result)
+names = []
+scores = {}
+
+# Scraping the names
+NAME_SKIP = 13
+NAME_OFFSET = 5
+iter = 0
+for element in root.iter("a"):
+    if (iter < NAME_SKIP):
+        pass
+    else:
+        if type(element.text) is str:
+            split_name = element.text.split(' ')
+            if len(split_name) > 1 and split_name[0] != "United" and split_name[0] != "Support":
+                names.append(' '.join(split_name))
+    iter += 1
+
+# Scraping the scores
+SCORE_SKIP = 4
+SCORE_OFFSET = 4
+iter = 0
+count = 0
+for element in root.iter("td"):
+    if (iter < SCORE_SKIP):
+        pass
+    else:
+        if ((iter - SCORE_SKIP + 1) % SCORE_OFFSET == 0):
+            scores[names[count]] = element.text
+            count += 1
+    iter += 1
+
+for name in names:
+    print(name, ':', scores[name])
